76343d5eda25b020c814f9d30f474b5f8d1ee0d4
kensk8er <kensk8er1017@gmail.com>
2017-07-18 19:58:39 +0100

    Updated parameters in random-search.

--- trainer.py
+++ trainer.py
@@ -69,12 +69,12 @@ def _get_parameter_space():
     """Define the parameter space to explore here."""
     parameter_space = OrderedDict()
     parameter_space.update({'embedding_size': [16 * i for i in range(1, 9)]})
-    parameter_space.update({'char_rnn_size': [32 * i for i in range(1, 30)]})
-    parameter_space.update({'word_rnn_size': [32 * i for i in range(1, 30)]})
+    parameter_space.update({'char_rnn_size': [128 * i for i in range(1, 11)]})
+    parameter_space.update({'word_rnn_size': [128 * i for i in range(1, 11)]})
     parameter_space.update({'learning_rate': [0.0001 * (2 ** i) for i in range(8)]})
-    parameter_space.update({'embedding_dropout': [0., 0.03, 0.05, 0.1]})
-    parameter_space.update({'char_rnn_dropout': [0., 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5]})
-    parameter_space.update({'word_rnn_dropout': [0., 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5]})
+    parameter_space.update({'embedding_dropout': [0., 0.01, 0.03, 0.05, 0.1]})
+    parameter_space.update({'char_rnn_dropout': [0., 0.01, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5]})
+    parameter_space.update({'word_rnn_dropout': [0., 0.01, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5]})
     return parameter_space
 
 
